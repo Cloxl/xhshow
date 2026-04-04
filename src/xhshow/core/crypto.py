83d04d20122b787b1ab9df9ca429f817626b3d50
Jack Li <ljc545w@qq.com>
2026-04-05 01:24:29 +0800

    Cancel the import of extract_api_path.

--- src/xhshow/core/crypto.py
+++ src/xhshow/core/crypto.py
@@ -8,7 +8,6 @@ from ..utils.bit_ops import BitOperations
 from ..utils.encoder import Base64Encoder
 from ..utils.hex_utils import HexProcessor
 from ..utils.random_gen import RandomGenerator
-# from ..utils.url_utils import extract_api_path
 
 if TYPE_CHECKING:
     from ..session import SignState
